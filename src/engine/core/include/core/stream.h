92b96324f89f30af93cbae9e4566cde8fe7c2233
rtx77 <gulnabd@mail.ru>
2021-12-16 18:09:25 +0300

    Добавлены копирайты для ветки windows_support.

--- src/engine/core/include/core/stream.h
+++ src/engine/core/include/core/stream.h
@@ -1,4 +1,5 @@
 /* Copyright © 2020 Evgeny Zaytsev <zx_90@mail.ru>
+ * Copyright © 2021 Abdullin Timur <abdtimurrif@gmail.com>
  * 
  * Distributed under the terms of the GNU LGPL v3 license. See accompanying
  * file LICENSE or copy at https://www.gnu.org/licenses/lgpl-3.0.html
--- src/engine/core/src/file_system_unix.c
+++ src/engine/core/src/file_system_unix.c
@@ -1,5 +1,6 @@
 /* Copyright © 2020,2021 Evgeny Zaytsev <zx_90@mail.ru>
- *
+ * Copyright © 2021 Abdullin Timur <abdtimurrif@gmail.com>
+ * 
  * Distributed under the terms of the GNU LGPL v3 license. See accompanying
  * file LICENSE or copy at https://www.gnu.org/licenses/lgpl-3.0.html
 */
