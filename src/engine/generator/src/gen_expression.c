a110bd1cff2ae1ce63da908562843011ef5c3680
zx-90 <zx_90@mail.ru>
2024-04-20 09:06:45 +0500

    Генератор. Добавлена операция умножение.

--- src/engine/generator/src/gen_expression.c
+++ src/engine/generator/src/gen_expression.c
@@ -852,6 +852,54 @@ static KarExpressionResult get_sign_minus(KarToken* token, KarLLVMData* llvmData
 	return kar_expression_result_none();
 }
 
+static KarExpressionResult get_sign_mul(KarToken* token, KarLLVMData* llvmData, KarString* moduleName, KarVars* vars, KarProjectErrorList* errors) {
+	KarToken* left = kar_token_child_get(token, 0);
+	KarExpressionResult leftRes = calc_expression(left, llvmData, moduleName, vars, errors);
+	KarToken* right = kar_token_child_get(token, 1);
+	KarExpressionResult rightRes = calc_expression(right, llvmData, moduleName, vars, errors);
+	if (!check_and_cast_types(&leftRes, &rightRes, llvmData, vars)) {
+		KarString* pathLeft = kar_vartree_create_full_path(get_reduced_type(leftRes.type, vars));
+		KarString* pathRight = kar_vartree_create_full_path(get_reduced_type(rightRes.type, vars));
+		KarString* errorText = kar_string_create_format("Операция умножение недопустима для типов \"%s\" и \"%s\".", pathLeft, pathRight);
+		kar_project_error_list_create_add(errors, moduleName, &token->cursor, 1, errorText);
+		KAR_FREE(pathLeft);
+		KAR_FREE(pathRight);
+		KAR_FREE(errorText);
+		return kar_expression_result_none();
+	}
+	if (leftRes.type == vars->standard.decimalType ||
+		leftRes.type == vars->standard.hexadecimalType ||
+		leftRes.type == vars->standard.int8Type ||
+		leftRes.type == vars->standard.int16Type ||
+		leftRes.type == vars->standard.int32Type ||
+		leftRes.type == vars->standard.int64Type ||
+		leftRes.type == vars->standard.unsigned8Type ||
+		leftRes.type == vars->standard.unsigned16Type ||
+		leftRes.type == vars->standard.unsigned32Type ||
+		leftRes.type == vars->standard.unsigned64Type
+	) {
+		KarExpressionResult res = kar_expression_result_none();
+		res.type = leftRes.type;
+		res.value = LLVMBuildMul(llvmData->builder, leftRes.value, rightRes.value, "sum");
+		return res;
+	}
+	if (leftRes.type == vars->standard.literalFloat ||
+		leftRes.type == vars->standard.float32Type ||
+		leftRes.type == vars->standard.float64Type
+	) {
+		KarExpressionResult res = kar_expression_result_none();
+		res.type = leftRes.type;
+		res.value = LLVMBuildFMul(llvmData->builder, leftRes.value, rightRes.value, "sum");
+		return res;
+	}
+	KarString* path = kar_vartree_create_full_path(get_reduced_type(leftRes.type, vars));
+	KarString* errorText = kar_string_create_format("Операция умножение недопустима для типа \"%s\".", path);
+	kar_project_error_list_create_add(errors, moduleName, &token->cursor, 1, errorText);
+	KAR_FREE(path);
+	KAR_FREE(errorText);
+	return kar_expression_result_none();
+}
+
 static KarExpressionResult calc_expression(KarToken* token, KarLLVMData* llvmData, KarString* moduleName, KarVars* vars, KarProjectErrorList* errors) {
 	// TODO: Проверить на компиляторе большое количество открывающихся и закрывающихся скобок.
 	switch (token->type) {
@@ -892,6 +940,7 @@ static KarExpressionResult calc_expression(KarToken* token, KarLLVMData* llvmDat
 	case (KAR_TOKEN_SIGN_SINGLE_MINUS): return get_sign_single_minus(token, llvmData, moduleName, vars, errors);
 	case (KAR_TOKEN_SIGN_PLUS): return get_sign_plus(token, llvmData, moduleName, vars, errors);
 	case (KAR_TOKEN_SIGN_MINUS): return get_sign_minus(token, llvmData, moduleName, vars, errors);
+	case (KAR_TOKEN_SIGN_MUL): return get_sign_mul(token, llvmData, moduleName, vars, errors);
 	default:
 		// TODO: В сообщении об ошибке добавить тип оператора.
 		kar_project_error_list_create_add(errors, moduleName, &token->cursor, 1, "Неизвестный оператор.");
