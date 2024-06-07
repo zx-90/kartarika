a10fbb2b38f445f0082c5261df47f760e89b0bac
zx-90 <zx_90@mail.ru>
2024-06-07 11:09:17 +0500

    Генератор. Добавлена операция больше или равно.

--- src/engine/generator/src/gen_expression.c
+++ src/engine/generator/src/gen_expression.c
@@ -1815,6 +1815,72 @@ static KarExpressionResult get_sign_greater(KarToken* token, KarLLVMData* llvmDa
 	return kar_expression_result_none();
 }
 
+static KarExpressionResult get_sign_greater_or_equal(KarToken* token, KarLLVMData* llvmData, KarString* moduleName, KarVars* vars, KarProjectErrorList* errors) {
+	KarToken* left = kar_token_child_get(token, 0);
+	KarExpressionResult leftRes = calc_expression(left, llvmData, moduleName, vars, errors);
+	KarToken* right = kar_token_child_get(token, 1);
+	KarExpressionResult rightRes = calc_expression(right, llvmData, moduleName, vars, errors);
+	if (!check_and_cast_types(&leftRes, &rightRes, llvmData, vars)) {
+		KarString* pathLeft = kar_vartree_create_full_path(get_reduced_type(leftRes.type, vars));
+		KarString* pathRight = kar_vartree_create_full_path(get_reduced_type(rightRes.type, vars));
+		KarString* errorText = kar_string_create_format("Операция равно недопустима для типов \"%s\" и \"%s\".", pathLeft, pathRight);
+		kar_project_error_list_create_add(errors, moduleName, &token->cursor, 1, errorText);
+		KAR_FREE(pathLeft);
+		KAR_FREE(pathRight);
+		KAR_FREE(errorText);
+		return kar_expression_result_none();
+	}
+
+	if (leftRes.type == vars->standard.decimalType ||
+		leftRes.type == vars->standard.int8Type ||
+		leftRes.type == vars->standard.int16Type ||
+		leftRes.type == vars->standard.int32Type ||
+		leftRes.type == vars->standard.int64Type
+	) {
+		KarExpressionResult res = kar_expression_result_none();
+		res.type = vars->standard.boolType;
+		res.value = LLVMBuildICmp(llvmData->builder, LLVMIntSGE, leftRes.value, rightRes.value, "var");
+		return res;
+	}
+
+	if (leftRes.type == vars->standard.hexadecimalType ||
+		leftRes.type == vars->standard.unsigned8Type ||
+		leftRes.type == vars->standard.unsigned16Type ||
+		leftRes.type == vars->standard.unsigned32Type ||
+		leftRes.type == vars->standard.unsigned64Type
+	) {
+		KarExpressionResult res = kar_expression_result_none();
+		res.type = vars->standard.boolType;
+		res.value = LLVMBuildICmp(llvmData->builder, LLVMIntUGE, leftRes.value, rightRes.value, "var");
+		return res;
+	}
+
+	if (leftRes.type == vars->standard.literalFloatType ||
+		leftRes.type == vars->standard.float32Type ||
+		leftRes.type == vars->standard.float64Type
+	) {
+		KarExpressionResult res = kar_expression_result_none();
+		res.type = vars->standard.boolType;
+		res.value = LLVMBuildFCmp(llvmData->builder, LLVMRealOGE, leftRes.value, rightRes.value, "var");
+		return res;
+	}
+
+	if (leftRes.type == vars->standard.stringType) {
+		KarExpressionResult res = kar_expression_result_none();
+		res.type = vars->standard.boolType;
+		LLVMValueRef in[] = {leftRes.value, rightRes.value};
+		res.value = LLVMBuildCall(llvmData->builder, llvmData->isGreaterOrEqualString, in, 2, "isEqualString");
+		return res;
+	}
+
+	KarString* path = kar_vartree_create_full_path(get_reduced_type(leftRes.type, vars));
+	KarString* errorText = kar_string_create_format("Операция равно недопустима для типа \"%s\".", path);
+	kar_project_error_list_create_add(errors, moduleName, &token->cursor, 1, errorText);
+	KAR_FREE(path);
+	KAR_FREE(errorText);
+	return kar_expression_result_none();
+}
+
 static KarExpressionResult calc_expression(KarToken* token, KarLLVMData* llvmData, KarString* moduleName, KarVars* vars, KarProjectErrorList* errors) {
 	// TODO: Проверить на компиляторе большое количество открывающихся и закрывающихся скобок.
 	switch (token->type) {
@@ -1869,6 +1935,7 @@ static KarExpressionResult calc_expression(KarToken* token, KarLLVMData* llvmDat
 	case (KAR_TOKEN_SIGN_EQUAL): return get_sign_equal(token, llvmData, moduleName, vars, errors);
 	case (KAR_TOKEN_SIGN_NOT_EQUAL): return get_sign_not_equal(token, llvmData, moduleName, vars, errors);
 	case (KAR_TOKEN_SIGN_GREATER): return get_sign_greater(token, llvmData, moduleName, vars, errors);
+	case (KAR_TOKEN_SIGN_GREATER_OR_EQUAL): return get_sign_greater_or_equal(token, llvmData, moduleName, vars, errors);
 	default:
 		// TODO: В сообщении об ошибке добавить тип оператора.
 		kar_project_error_list_create_add(errors, moduleName, &token->cursor, 1, "Неизвестный оператор.");
